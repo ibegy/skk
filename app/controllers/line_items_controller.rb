7e932324e514d70b160c1d12757487ce03874b22
ibegy <i.begovic90@gmail.com>
2019-03-29 10:25:22 +0100

    Added cart icon and ticket amount reduction functionality

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,16 +27,24 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     ticket = Ticket.find(params[:ticket_id])
-    @line_item = @cart.add_ticket(ticket)
-    ticket.update_attribute(:amount, ticket.amount)
 
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'You bought the ticket!' }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+    if ticket.amount == 0
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: 'Out of stock! :(' }
+        format.json { head :no_content }
+      end
+    else
+      @line_item = @cart.add_ticket(ticket)
+      ticket.update_attribute(:amount, ticket.amount)
+
+      respond_to do |format|
+        if @line_item.save
+          format.html { redirect_to @line_item.cart, notice: 'You bought the ticket!' }
+          format.json { render :show, status: :created, location: @line_item }
+        else
+          format.html { render :new }
+          format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
